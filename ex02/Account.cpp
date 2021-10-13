15a400095d2d7f07ca7a3c58150ebda1fb5f5a59
mfunyu <mfunyu@student.42tokyo.jp>
2021-10-14 02:03:17 +0900

    [update] compilation works

---
+++ ex02/Account.cpp
@@ -0,0 +1,39 @@
+// /* ************************************************************************** */
+// /*                                                                            */
+// /*                                                        :::      ::::::::   */
+// /*   Account.cpp                                        :+:      :+:    :+:   */
+// /*                                                    +:+ +:+         +:+     */
+// /*   By: mfunyu <mfunyu@student.42tokyo.jp>         +#+  +:+       +#+        */
+// /*                                                +#+#+#+#+#+   +#+           */
+// /*   Created: 2021/10/14 01:41:56 by mfunyu            #+#    #+#             */
+// /*   Updated: 2021/10/14 01:47:59 by mfunyu           ###   ########.fr       */
+// /*                                                                            */
+// /* ************************************************************************** */
+
+#include "Account.hpp"
+
+void	Account::displayAccountsInfos( void ) {
+
+}
+
+void	Account::makeDeposit( int deposit ) {
+	(void)deposit;
+}
+
+bool	Account::makeWithdrawal( int withdrawal ) {
+	(void)withdrawal;
+	return true;
+}
+
+void	Account::displayStatus( void ) const {
+
+}
+
+Account::Account( int initial_deposit ) {
+	(void)initial_deposit;
+	return;
+}
+
+Account::~Account( void ) {
+	return;
+}
