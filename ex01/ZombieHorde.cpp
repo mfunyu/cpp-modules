844dcc3fe8ab79f7fe1babccf059e54b31987bf7
mfunyu <mfunyu@student.42tokyo.jp>
2021-10-17 19:08:29 +0900

    ex01 [add] new func instructions

---
+++ ex01/ZombieHorde.cpp
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ZombieHorde.cpp                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mfunyu <mfunyu@student.42tokyo.jp>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/10/17 19:02:02 by mfunyu            #+#    #+#             */
+/*   Updated: 2021/10/17 19:07:55 by mfunyu           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Zombie.hpp"
+#include <iostream>
+
+Zombie*	zombieHorde( int N, std::string name ){
+	// allocates N Zombie objects in a single allocation
+	// initialize each Zombie by giving to each one a name
+	// return the pointer to the first Zombie
+}
