fe0e2f03c9821c9a56d98ef5af1ebb8b2f5c813d
mfunyu <mfunyu@student.42tokyo.jp>
2021-10-23 16:42:52 +0900

    ex04 [update] main.cpp

--- ex04/main.cpp
+++ ex04/main.cpp
@@ -6,7 +6,7 @@
 /*   By: mfunyu <mfunyu@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/23 15:07:12 by mfunyu            #+#    #+#             */
-/*   Updated: 2021/10/23 16:35:23 by mfunyu           ###   ########.fr       */
+/*   Updated: 2021/10/23 16:42:21 by mfunyu           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,14 +17,24 @@ void	replace(std::string filename, std::string s1, std::string s2);
 
 int		main(int ac, char **av)
 {
-	if (ac < 4) {
-		std::cout << "required 3 arguments" << std::endl;
+	if (ac < 2) {
+		std::cout << "filename required" << std::endl;
 		return 0;
 	}
 
 	std::string		filename = av[1];
-	std::string		s1 = av[2];
-	std::string		s2 = av[3];
+	std::string		s1;
+	std::string		s2;
+
+	if (ac > 3) {
+		/* from argv */
+		s1 = av[2];
+		s2 = av[3];
+	} else {
+		s1 = ")\n";
+		s2 = "\t0000000000000000\t";
+	}
+
 	replace(filename, s1, s2);
 	return 0;
 }
