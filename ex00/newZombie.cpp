c2b1a0c93be715657a2c9d6a5326ea9d213bc64b
mfunyu <mfunyu@student.42tokyo.jp>
2021-10-24 00:46:56 +0900

    ex00 [clean] refactor new/random

--- ex00/newZombie.cpp
+++ ex00/newZombie.cpp
@@ -6,17 +6,16 @@
 /*   By: mfunyu <mfunyu@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/12 23:58:55 by mfunyu            #+#    #+#             */
-/*   Updated: 2021/10/23 22:29:57 by mfunyu           ###   ########.fr       */
+/*   Updated: 2021/10/24 00:46:00 by mfunyu           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Zombie.hpp"
 #include <string>
 
-Zombie* newZombie( std::string name ) {
-	Zombie *new_zombie;
-
-	new_zombie = new Zombie( name );
+Zombie* newZombie( std::string name )
+{
+	Zombie *new_zombie = new Zombie( name );
 
 	return (new_zombie);
 }
--- ex00/randomChump.cpp
+++ ex00/randomChump.cpp
@@ -6,16 +6,16 @@
 /*   By: mfunyu <mfunyu@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/12 23:59:43 by mfunyu            #+#    #+#             */
-/*   Updated: 2021/10/23 22:30:12 by mfunyu           ###   ########.fr       */
+/*   Updated: 2021/10/24 00:46:07 by mfunyu           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Zombie.hpp"
 #include <string>
 
-void randomChump( std::string name ) {
-
+void randomChump( std::string name )
+{
 	Zombie	random_zombie(name);
 
 	random_zombie.announce();
-};
+}
