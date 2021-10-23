c590a83b2678b63993fefbe23033e79eb71e1ff9
mfunyu <mfunyu@student.42tokyo.jp>
2021-10-23 20:24:16 +0900

    ex05 [update] use if/elseif/else

--- ex05/Karen.cpp
+++ ex05/Karen.cpp
@@ -6,33 +6,49 @@
 /*   By: mfunyu <mfunyu@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/23 16:50:35 by mfunyu            #+#    #+#             */
-/*   Updated: 2021/10/23 17:00:27 by mfunyu           ###   ########.fr       */
+/*   Updated: 2021/10/23 20:23:21 by mfunyu           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Karen.hpp"
 #include <iostream>
 
-void	Karen::complain( std::string level ) {
-	
+struct complain_types
+{
+	std::string		level;
+	void (Karen::*FuncPtr)();
+};
 
+void	Karen::complain( std::string level ) {
+	typedef		void (Karen::*FuncPtr)();
+	FuncPtr		func;
+
+	if (level.compare("debug") == 0)
+		func = &Karen::debug;
+	if (level.compare("info") == 0)
+		func = &Karen::info;
+	if (level.compare("warning") == 0)
+		func = &Karen::warning;
+	if (level.compare("error") == 0)
+		func = &Karen::error;
+	(this->*func)();
 }
 
 
 void	Karen::debug( void ) {
-	std::cout << "I love to get extra bacon for my 7XL-double-cheese-triple-pickle-special-ketchup burger. I just love it!" << std::endl;
+	std::cout << "DEBUG I love to get extra bacon for my 7XL-double-cheese-triple-pickle-special-ketchup burger. I just love it!" << std::endl;
 }
 
 void	Karen::info( void ) {
-	std::cout << "I cannot believe adding extra bacon cost more money. You don’t put enough! If you did I would not have to ask for it!" << std::endl;
+	std::cout << "INFO I cannot believe adding extra bacon cost more money. You don’t put enough! If you did I would not have to ask for it!" << std::endl;
 }
 
 void	Karen::warning( void ) {
-	std::cout << "I think I deserve to have some extra bacon for free. I’ve been coming here for years and you just started working here last month." << std::endl;
+	std::cout << "WARNING I think I deserve to have some extra bacon for free. I’ve been coming here for years and you just started working here last month." << std::endl;
 }
 
 void	Karen::error( void ) {
-	std::cout << "This is unacceptable, I want to speak to the manager now." << std::endl;
+	std::cout << "ERROR This is unacceptable, I want to speak to the manager now." << std::endl;
 }
 
 Karen::Karen() {
