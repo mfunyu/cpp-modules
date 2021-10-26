20fef32a69adc7800cd996e7657636bc1df76d12
mfunyu <mfunyu@student.42tokyo.jp>
2021-10-27 00:35:55 +0900

    ex02 [add] advanced test

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -6,12 +6,70 @@
 /*   By: mfunyu <mfunyu@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/25 19:15:23 by mfunyu            #+#    #+#             */
-/*   Updated: 2021/10/26 15:37:57 by mfunyu           ###   ########.fr       */
+/*   Updated: 2021/10/27 00:35:34 by mfunyu           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Fixed.hpp"
 #include <iostream>
+
+void	test_arithmatics(Fixed const &a, Fixed const &b)
+{
+	std::cout << "====== arithmatics ========" << std::endl;
+	std::cout << a << " + " << b << " = " << a + b << std::endl;
+	std::cout << a << " - " << b << " = " << a - b << std::endl;
+	std::cout << a << " * " << b << " = " << a * b << std::endl;
+	std::cout << a << " / " << b << " = " << a / b << std::endl;
+}
+
+void	test_comparisons(Fixed const &a, Fixed const &b)
+{
+	std::cout << "====== comparisons ========" << std::endl;
+	std::cout << a << " < " << b << ": " << (a < b) << std::endl;
+	std::cout << a << " > " << b << ": " << (a > b) << std::endl;
+	std::cout << a << " >= " << b << ": " << (a >= b) << std::endl;
+	std::cout << a << " <= " << b << ": " << (a <= b) << std::endl;
+	std::cout << a << " == " << b << ": " << (a == b) << std::endl;
+	std::cout << a << " != " << b << ": " << (a != b) << std::endl;
+}
+
+void	test_inc_dec(Fixed &a)
+{
+	std::cout << "====== inc / dec ========" << std::endl;
+	std::cout << "a  : " << a << std::endl;
+	std::cout << "++a: " << ++a << std::endl;
+	std::cout << "a  : " << a << std::endl;
+	std::cout << "a++: " << a++ << std::endl;
+	std::cout << "a  : " << a << std::endl;
+	std::cout << "--a: " << --a << std::endl;
+	std::cout << "a  : " << a << std::endl;
+	std::cout << "a--: " << a-- << std::endl;
+	std::cout << "a  : " << a << std::endl;
+}
+
+void	test_min_max(Fixed a, Fixed b)
+{
+	std::cout << "====== min / max ========" << std::endl;
+	std::cout << "min " << Fixed::min(a, b) << "	(a = " << a << " b = " << b << std::endl;
+	std::cout << "max " << Fixed::max(a, b) << "	(a = " << a << " b = " << b << std::endl;
+}
+
+void	test_min_max_const(Fixed const a, Fixed const b)
+{
+	std::cout << "const min " << Fixed::min(a, b) << "	(a = " << a << " b = " << b << std::endl;
+	std::cout << "const max " << Fixed::max(a, b) << "	(a = " << a << " b = " << b << std::endl;
+}
+
+void	test_all(Fixed a, Fixed b)
+{
+	test_arithmatics(a, b);
+	test_comparisons(a, b);
+	test_inc_dec(a);
+	test_inc_dec(b);
+	test_min_max(a, b);
+	test_min_max_const(a, b);
+	std::cout << std::endl;
+}
 
 int		main(void)
 {
@@ -25,6 +83,17 @@ int		main(void)
 	std::cout << a << std::endl;
 	std::cout << b << std::endl;
 	std::cout << Fixed::max( a, b ) << std::endl;
+
+	#ifdef ADVANCED
+	{
+		test_all(Fixed(10), Fixed(5));
+		test_all(Fixed(20), Fixed(20));
+		test_all(Fixed(0), Fixed(200));
+		test_all(Fixed(200), Fixed(0));
+		test_all(Fixed(-42.3f), Fixed(42.3f));
+		test_all(Fixed(-214748364), Fixed(2147483647));
+	}
+	#endif
 
 	return 0;
 }
