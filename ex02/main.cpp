55829013eec8815ed0ba0971bb53578cb2b20f23
mfunyu <mfunyu@student.42tokyo.jp>
2021-10-29 13:13:28 +0900

    ex02 [update] main scope

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -6,34 +6,40 @@
 /*   By: mfunyu <mfunyu@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/27 12:52:31 by mfunyu            #+#    #+#             */
-/*   Updated: 2021/10/29 00:37:56 by mfunyu           ###   ########.fr       */
+/*   Updated: 2021/10/29 13:12:49 by mfunyu           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ClapTrap.hpp"
 #include "ScavTrap.hpp"
 #include "FragTrap.hpp"
+#include <iostream>
 
 int		main()
 {
-	/*
-	ClapTrap	claptrap("Sum");
+	{
+		ClapTrap	claptrap("Sum");
 
-	claptrap.attack("Jhon");
-	claptrap.beRepaired(42);
-	claptrap.takeDamage(100);
+		claptrap.attack("Jhon");
+		claptrap.beRepaired(42);
+		claptrap.takeDamage(100);
+	}
+	std::cout << std::endl;
+	{
+		ScavTrap	scavtrap("Nany");
 
-	ScavTrap	scavtrap("Nany");
+		scavtrap.attack("Jhon");
+		scavtrap.beRepaired(42);
+		scavtrap.guardGate();
+		scavtrap.takeDamage(100);
+	}
+	std::cout << std::endl;
+	{
+		FragTrap	fragtrap("Bob");
 
-	scavtrap.attack("Jhon");
-	scavtrap.beRepaired(42);
-	scavtrap.guardGate();
-	scavtrap.takeDamage(100);
-	*/
-	FragTrap	fragtrap("Bob");
-
-	fragtrap.attack("Julia");
-	fragtrap.highFivesGuys();
-	fragtrap.takeDamage(50);
-	fragtrap.beRepaired(20);
+		fragtrap.attack("Julia");
+		fragtrap.highFivesGuys();
+		fragtrap.takeDamage(50);
+		fragtrap.beRepaired(20);
+	}
 }
