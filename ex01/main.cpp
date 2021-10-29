180951d718c30042bc915877b5bec99a144b6716
mfunyu <mfunyu@student.42tokyo.jp>
2021-10-29 12:05:56 +0900

    ex01 [update] main add scope

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -6,25 +6,30 @@
 /*   By: mfunyu <mfunyu@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/27 12:52:31 by mfunyu            #+#    #+#             */
-/*   Updated: 2021/10/28 16:41:59 by mfunyu           ###   ########.fr       */
+/*   Updated: 2021/10/29 11:58:43 by mfunyu           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ClapTrap.hpp"
 #include "ScavTrap.hpp"
+#include <iostream>
 
 int		main()
 {
-	// ClapTrap	claptrap("Sum");
+	{
+		ClapTrap	claptrap("Sum");
 
-	// claptrap.attack("Jhon");
-	// claptrap.beRepaired(42);
-	// claptrap.takeDamage(100);
+		claptrap.attack("Jhon");
+		claptrap.beRepaired(40);
+		claptrap.takeDamage(100);
+	}
+	std::cout << std::endl;
+	{
+		ScavTrap	scavtrap("Nancy");
 
-	ScavTrap	scavtrap("Nany");
-
-	scavtrap.attack("Jhon");
-	scavtrap.beRepaired(42);
-	scavtrap.guardGate();
-	scavtrap.takeDamage(100);
+		scavtrap.attack("Jhon");
+		scavtrap.beRepaired(80);
+		scavtrap.guardGate();
+		scavtrap.takeDamage(100);
+	}
 }
