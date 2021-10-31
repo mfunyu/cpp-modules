249c91729b539e33ed5dd043a33dca20915d83b5
mfunyu <mfunyu@student.42tokyo.jp>
2021-10-31 22:20:23 +0900

    ex03 [add] Interfaces

--- ex03/ICharacter.hpp
+++ ex03/ICharacter.hpp
@@ -6,22 +6,21 @@
 /*   By: mfunyu <mfunyu@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/31 14:48:54 by mfunyu            #+#    #+#             */
-/*   Updated: 2021/10/31 14:48:55 by mfunyu           ###   ########.fr       */
+/*   Updated: 2021/10/31 22:16:42 by mfunyu           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef ICHARACTER_HPP
 # define ICHARACTER_HPP
 
+# include <string>
+
+class AMateria;
+
 class ICharacter
 {
-	private:
-
 	public:
-		ICharacter();
-		virtual ~ICharacter();
-		ICharacter(ICharacter const &other);
-		ICharacter	&operator=(ICharacter const &other);
+		virtual ~ICharacter() {}
 
 		virtual std::string const & getName() const = 0;
 		virtual void equip(AMateria* m) = 0;
--- ex03/IMateriaSource.hpp
+++ ex03/IMateriaSource.hpp
@@ -6,20 +6,21 @@
 /*   By: mfunyu <mfunyu@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/31 14:48:50 by mfunyu            #+#    #+#             */
-/*   Updated: 2021/10/31 14:50:21 by mfunyu           ###   ########.fr       */
+/*   Updated: 2021/10/31 22:18:16 by mfunyu           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef IMATERIASOURCE_HPP
 # define IMATERIASOURCE_HPP
 
+#include <string>
+
+class AMateria;
+
 class IMateriaSource
 {
 	public:
-		IMateriaSource();
 		virtual ~IMateriaSource() {}
-		IMateriaSource(IMateriaSource const &other);
-		IMateriaSource	&operator=(IMateriaSource const &other);
 
 		virtual void learnMateria(AMateria*) = 0;
 		virtual AMateria* createMateria(std::string const & type) = 0;
