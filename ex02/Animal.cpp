a6df053c21b4540f3446ffee5cea06cdc3bbe9a8
mfunyu <mfunyu@student.42tokyo.jp>
2021-10-31 14:01:55 +0900

    ex02 [add] Animal to Abstract Class

--- ex02/Animal.cpp
+++ ex02/Animal.cpp
@@ -1,1 +1,46 @@
-../ex01/Animal.cpp
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Animal.cpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mfunyu <mfunyu@student.42tokyo.jp>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/10/29 14:55:24 by mfunyu            #+#    #+#             */
+/*   Updated: 2021/10/31 14:02:41 by mfunyu           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Animal.hpp"
+#include <iostream>
+
+Animal::Animal()
+{
+	std::cout << "Animal default constructor called" << std::endl;
+}
+
+Animal::~Animal()
+{
+	std::cout << "Animal destructor called" << std::endl;
+}
+
+Animal::Animal(const Animal &other)
+{
+	*this = other;
+}
+
+Animal	&Animal::operator=(const Animal &other)
+{
+	if (this != &other)
+	{
+	}
+	return *this;
+}
+
+Animal::Animal(std::string type) : _type(type)
+{
+}
+
+std::string		Animal::getType() const
+{
+	return _type;
+}
