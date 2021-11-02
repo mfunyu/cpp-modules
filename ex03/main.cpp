905ab4fdba0564c9888388e64ad2e2d9ab8930e6
mfunyu <mfunyu@student.42tokyo.jp>
2021-11-02 11:26:11 +0900

    ex03 [update] main test_learnMateria

--- ex03/main.cpp
+++ ex03/main.cpp
@@ -6,7 +6,7 @@
 /*   By: mfunyu <mfunyu@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/31 14:44:24 by mfunyu            #+#    #+#             */
-/*   Updated: 2021/11/01 21:50:53 by mfunyu           ###   ########.fr       */
+/*   Updated: 2021/11/02 11:25:50 by mfunyu           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,34 @@
 #include "Character.hpp"
 #include "MateriaSource.hpp"
 #include <iostream>
+
+void	test_learnMateria()
+{
+	IMateriaSource* src = new MateriaSource();
+	src->learnMateria(new Cure());
+	src->learnMateria(NULL);
+	src->learnMateria(new Cure());
+	src->learnMateria(new Cure());
+	src->learnMateria(new Ice());
+	src->learnMateria(new Cure());
+
+	ICharacter* me = new Character("me");
+
+	AMateria* tmp;
+	tmp = src->createMateria("ice");
+	me->equip(tmp);
+	tmp = src->createMateria("cure");
+	me->equip(tmp);
+
+	ICharacter* nop = new Character("nop");
+
+	me->use(0, *nop);
+	me->use(1, *nop);
+
+	delete nop;
+	delete me;
+	delete src;
+}
 
 void	test_createMateria()
 {
@@ -78,6 +106,8 @@ int main()
 	std::cout << std::endl;
 	{
 		test_createMateria();
+		std::cout << std::endl;
+		test_learnMateria();
 	}
 	return 0;
 }
