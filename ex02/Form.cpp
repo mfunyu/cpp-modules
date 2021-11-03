ea346abd1b39964317710a106ca57efecb25abaf
mfunyu <mfunyu@student.42tokyo.jp>
2021-11-03 21:52:31 +0900

    ex02 [add] class From

---
+++ ex02/Form.cpp
@@ -0,0 +1,115 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Form.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mfunyu <mfunyu@student.42tokyo.jp>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/03 13:49:13 by mfunyu            #+#    #+#             */
+/*   Updated: 2021/11/03 20:52:05 by mfunyu           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Form.hpp"
+#include <string>
+#include <iostream>
+
+Form::Form() : _isSigned(false), _gradeToSign(GRADE_HIGHEST), _gradeToExec(GRADE_HIGHEST)
+{
+}
+
+Form::~Form()
+{
+}
+
+Form::Form(const Form &other) :
+	_name(other._name), _gradeToSign(other._gradeToSign), _gradeToExec(other._gradeToExec)
+{
+	*this = other;
+}
+
+Form	&Form::operator=(const Form &other)
+{
+	if (this != &other)
+	{
+		_isSigned = other._isSigned;
+	}
+	return *this;
+}
+
+Form::Form(std::string name, int gradeToSign, int gradeToExec) :
+	_name(name), _isSigned(false), _gradeToSign(validateGrade(gradeToSign)), _gradeToExec(validateGrade(gradeToExec))
+{
+}
+
+std::string		Form::getName() const
+{
+	return _name;
+}
+
+bool	Form::getIsSigned() const
+{
+	return _isSigned;
+}
+
+int		Form::getGradeToSign() const
+{
+	return _gradeToSign;
+}
+
+int		Form::getGradeToExec() const
+{
+	return _gradeToExec;
+}
+
+void	Form::beSigned(Bureaucrat const &bureaucrat)
+{
+	if (_gradeToSign < bureaucrat.getGrade())
+	{
+		throw GradeTooLowException();
+	}
+	_isSigned = true;
+}
+
+bool	Form::isGradeTooHigh(int grade)
+{
+	return grade < GRADE_HIGHEST;
+}
+
+bool	Form::isGradeTooLow(int grade)
+{
+	return GRADE_LOWEST < grade;
+}
+
+int		Form::validateGrade(int grade)
+{
+	if (isGradeTooHigh(grade))
+	{
+		throw GradeTooHighException();
+	}
+	if (isGradeTooLow(grade))
+	{
+		throw GradeTooLowException();
+	}
+	return grade;
+}
+
+const char* Form::GradeTooHighException::what() const throw()
+{
+	return "Grade is too high";
+}
+
+const char*	Form::GradeTooLowException::what() const throw()
+{
+	return "Grade is too low";
+}
+
+
+std::ostream	&operator<<(std::ostream &os, const Form& form)
+{
+	std::cout << form.getName() <<\
+				 ": grade to sign = " << form.getGradeToSign() <<\
+				 ", grade to execute = " << form.getGradeToExec() <<\
+				 ", signed = " << (form.getIsSigned() ? "true" : "false");
+	return os;
+}
---
+++ ex02/Form.hpp
@@ -0,0 +1,49 @@
+#ifndef FORM_HPP
+# define FORM_HPP
+
+# include <string>
+
+class Form;
+# include "Bureaucrat.hpp"
+
+class Form
+{
+	private:
+		std::string const	_name;
+		bool				_isSigned;
+		int const			_gradeToSign;
+		int const			_gradeToExec;
+		int					validateGrade(int grade);
+
+	public:
+		Form();
+		~Form();
+		Form(Form const &other);
+		Form	&operator=(Form const &other);
+
+		Form(std::string name, int gradeToSign, int gradeToExec);
+
+		std::string		getName() const;
+		bool			getIsSigned() const;
+		int				getGradeToSign() const;
+		int				getGradeToExec() const;
+
+		void			beSigned(Bureaucrat const &bureaucrat);
+
+		static bool		isGradeTooHigh(int grade);
+		static bool		isGradeTooLow(int grade);
+
+		class GradeTooHighException : public std::exception
+		{
+			const char* what() const throw();
+		};
+		class GradeTooLowException : public std::exception
+		{
+			const char* what() const throw();
+		};
+};
+
+
+std::ostream	&operator<<(std::ostream &os, const Form& form);
+
+#endif /* FORM_HPP */
