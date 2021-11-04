0d5fcd2d207490b2907c00917b0b78c9ff76e0c3
mfunyu <mfunyu@student.42tokyo.jp>
2021-11-04 17:57:05 +0900

    ex03 [add] main

---
+++ ex03/main.cpp
@@ -0,0 +1,70 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mfunyu <mfunyu@student.42tokyo.jp>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/04 15:56:17 by mfunyu            #+#    #+#             */
+/*   Updated: 2021/11/04 17:55:13 by mfunyu           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <string>
+#include "Intern.hpp"
+#include "Form.hpp"
+
+/****************** FOR PRINT and FORMATTING ******************/
+
+# define SET_COLOR "\033[36m"
+# define RESET_COLOR "\033[m"
+
+void	printTestName(std::string testName)
+{
+	std::cout << SET_COLOR "\n/*** Test " << testName <<\
+				" ***/" RESET_COLOR << std::endl;
+}
+
+int		main()
+{
+	{
+		printTestName("Robotomy Request Form");
+		Intern		someRandomIntern;
+		Form*		rrf;
+		std::string	target = "Bender";
+
+		rrf = someRandomIntern.makeForm("robotomy request", target);
+		std::cout << *rrf << std::endl;
+		delete rrf;
+	}
+	{
+		printTestName("Presidential Pardon Form");
+		Intern		someRandomIntern;
+		Form*		rrf;
+		std::string	target = "Lucy";
+
+		rrf = someRandomIntern.makeForm("presidential pardon", target);
+		std::cout << *rrf << std::endl;
+		delete rrf;
+	}
+	{
+		printTestName("Shrubbery Creation Form");
+		Intern		someRandomIntern;
+		Form*		rrf;
+		std::string	target = "Sum";
+
+		rrf = someRandomIntern.makeForm("shrubbery creation", target);
+		std::cout << *rrf << std::endl;
+		delete rrf;
+	}
+	{
+		printTestName("Wrong Form");
+		Intern		someRandomIntern;
+		Form*		rrf;
+		std::string	target = "Bender";
+
+		rrf = someRandomIntern.makeForm("non", target);
+		std::cout << rrf << std::endl;
+	}
+}
