4b4e54d699ad3cc6d447c1a4e2e5054b304531ce
mfunyu <mfunyu@student.42tokyo.jp>
2021-11-06 20:44:21 +0900

    ex01 [add] main: serialize deserialize

---
+++ ex01/main.cpp
@@ -0,0 +1,69 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mfunyu <mfunyu@student.42tokyo.jp>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/06 17:10:50 by mfunyu            #+#    #+#             */
+/*   Updated: 2021/11/06 20:43:06 by mfunyu           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Data.hpp"
+#include <cstdint>
+#include <iostream>
+
+/****************** FOR PRINT and FORMATTING ******************/
+
+# define SET_COLOR "\033[36m"
+# define RESET_COLOR "\033[m"
+
+void	printTestInfo(std::string testContent)
+{
+	std::cout << SET_COLOR "/*** Test: " << testContent <<\
+				" ***/" RESET_COLOR << std::endl;
+}
+
+/******************    Requied Functions    ******************/
+
+uintptr_t serialize(Data* ptr)
+{
+	return reinterpret_cast<uintptr_t>(ptr);
+}
+
+Data* deserialize(uintptr_t raw)
+{
+	return reinterpret_cast<Data *>(raw);
+}
+
+int		main()
+{
+	{
+		printTestInfo("Data test(\"test\");");
+
+		Data	test("test");
+
+		std::cout << "Ptr Before: " << &test << std::endl;
+
+		uintptr_t	ptr = serialize(&test);
+		Data*		deserialized = deserialize(ptr);
+
+		std::cout << "Ptr After : " << deserialized << std::endl;
+	}
+	std::cout << std::endl;
+	{
+		printTestInfo("Data* test = new Data(\"content\");");
+
+		Data*	test = new Data("content");
+
+		std::cout << "Ptr Before: " << test << std::endl;
+
+		uintptr_t	ptr = serialize(test);
+		Data*		deserialized = deserialize(ptr);
+
+		std::cout << "Ptr After : " << deserialized << std::endl;
+
+		delete deserialized;
+	}
+}
