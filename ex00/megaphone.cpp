ea62c0373a7219010881c4d4470abf1fd0330afa
mfunyu <mfunyu@student.42tokyo.jp>
2021-11-08 09:44:34 +0900

    ex00 [update] megaphone.c to more C++ like

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -6,28 +6,36 @@
 /*   By: mfunyu <mfunyu@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/01 01:45:58 by mfunyu            #+#    #+#             */
-/*   Updated: 2021/10/21 14:03:48 by mfunyu           ###   ########.fr       */
+/*   Updated: 2021/11/08 09:43:09 by mfunyu           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
-#include <cctype>
+#include <string>
+#include <locale>
 
-static void	_str_toupper(const char *str)
-{
-	for (int i = 0; str[i]; i++)
-		std::cout << std::toupper(str[i], std::locale());
-}
+namespace {
+	void	strToUpper(std::string const & str)
+	{
+		std::locale	locale;
+
+		for (int i = 0; str[i]; i++) {
+			std::cout << std::toupper(str[i], locale);
+		}
+	}
+} /* namespace */
 
 int main(int ac, char **av)
 {
-	if (ac <= 1)
-	{
+	if (ac <= 1) {
 		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
-		return (0);
+		return 0;
 	}
-	for (int i = 1; av[i]; i++)
-		_str_toupper(av[i]);
+
+	for (int i = 1; av[i]; i++) {
+		strToUpper(av[i]);
+	}
 	std::cout << std::endl;
-	return (0);
+
+	return 0;
 }
