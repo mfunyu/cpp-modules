9f258f4ab77e97769f268e60657027e7624e62f9
mfunyu <mfunyu@student.42tokyo.jp>
2021-11-09 23:09:56 +0900

    ex02 [update] template

--- ex02/Array.hpp
+++ ex02/Array.hpp
@@ -6,7 +6,7 @@
 /*   By: mfunyu <mfunyu@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/07 18:32:45 by mfunyu            #+#    #+#             */
-/*   Updated: 2021/11/09 23:01:55 by mfunyu           ###   ########.fr       */
+/*   Updated: 2021/11/09 23:08:10 by mfunyu           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,48 +18,50 @@
 template <typename T>
 class Array
 {
-	private:
-		T*				_array;
-		unsigned int	_size;
+  private:
+	T*				_array;
+	unsigned int	_size;
 
-	public:
-		Array<T>() : _size(0) {}
-		Array<T>(unsigned int n) : _size(n) {
-			_array = new T[n]();
+  public:
+	Array<T>() : _size(0) {}
+	Array<T>(unsigned int n) : _size(n) {
+		_array = new T[n]();
+	}
+
+	Array<T>(Array<T> const &other) {
+		_array = new T[other._size]();
+		_size = other._size;
+		for (unsigned int i = 0; i < other._size; i++) {
+			_array[i] = other._array[i];
 		}
+	}
 
-		Array<T>(Array<T> const &other) {
+	Array<T>	&operator=(Array<T> const &other) {
+		if (this != &other) {
+			delete [] _array;
 			_array = new T[other._size]();
 			_size = other._size;
 			for (unsigned int i = 0; i < other._size; i++) {
 				_array[i] = other._array[i];
 			}
 		}
+		return *this;
+	}
 
-		Array<T>	&operator=(Array<T> const &other) {
-			if (this != &other) {
-				delete [] _array;
-				_array = new T[other._size]();
-				_size = other._size;
-				for (unsigned int i = 0; i < other._size; i++) {
-					_array[i] = other._array[i];
-				}
-			}
-			return *this;
+	T	&operator[](unsigned int i) {
+		if (i < 0 || i >= _size) {
+			throw std::out_of_range("out of range");
 		}
+		return _array[i];
+	}
 
-		T	&operator[](unsigned int i) {
-			if (i < 0 || i >= _size) {
-				throw std::out_of_range("out of range");
-			}
-			return _array[i];
-		}
+	unsigned int	size(void) const {
+		return _size;
+	}
 
-		unsigned int	size() const {
-			return _size;
-		}
-
-		~Array<T>() {}
+	~Array<T>() {
+		delete [] _array;
+	}
 };
 
 #endif /* ARRAY_HPP */
