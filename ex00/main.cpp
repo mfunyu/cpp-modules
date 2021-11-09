9da5a54ebf4be1e94b4db0885b0ed818bab2c3f9
mfunyu <mfunyu@student.42tokyo.jp>
2021-11-09 13:23:18 +0900

    ex00 [update] main color

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -6,12 +6,20 @@
 /*   By: mfunyu <mfunyu@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/07 00:14:55 by mfunyu            #+#    #+#             */
-/*   Updated: 2021/11/07 18:26:13 by mfunyu           ###   ########.fr       */
+/*   Updated: 2021/11/09 13:21:52 by mfunyu           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 #include "whatever.hpp"
+
+#define COLOR_CYAN "\033[36m"
+#define COLOR_RESET "\033[0m"
+
+void	printTestName(std::string test){
+	std::cout << COLOR_CYAN << "****** " << test <<\
+				 " ******" << COLOR_RESET << std::endl;
+}
 
 int		subject_main() {
 
@@ -36,32 +44,32 @@ int		subject_main() {
 
 int		main()
 {
-	std::cout << "**** Subject Main ****" << std::endl;
+	printTestName("Subject Main");
 	subject_main();
 	std::cout << std::endl;
 
-	std::cout << "**** MAX ****" << std::endl;
+	printTestName("MAX");
 	std::cout << "max(1, 2) = " << max(1, 2) << std::endl;
 	std::cout << "max(42, 42) = " << max(42, 42) << std::endl;
 	std::cout << "max(1.51, 1.5) = " << max(1.51, 1.5) << std::endl;
 	std::cout << "max(10.001f, 10.002f) = " << max(10.001f, 10.002f) << std::endl;
 	std::cout << std::endl;
 
-	std::cout << "**** MIN ****" << std::endl;
+	printTestName("MIN");
 	std::cout << "min(1, 2) = " << min(1, 2) << std::endl;
 	std::cout << "min(-999.9, -1000.0) = " << min(-999.9, -1000.0) << std::endl;
 	std::cout << "min('a', 'b') = " << min('a', 'b') << std::endl;
 	std::cout << "min(\"42tokyo\", \"42Tokyo\") = " << min("42tokyo", "42Tokyo") << std::endl;
 	std::cout << std::endl;
 
-	std::cout << "**** SWAP ****" << std::endl;
+	printTestName("SWAP");
 	/* ft <-> tf */
 	int		ft = 42;
 	int		tf = 24;
 
 	std::cout << "before: ft = " << ft << std::endl;
 	std::cout << "before: tf = " << tf << std::endl;
-	std::cout << "swap(ft, tf)" << std::endl;
+	std::cout << "** swap(ft, tf) **" << std::endl;
 	swap(ft, tf);
 	std::cout << "after : ft = " << ft << std::endl;
 	std::cout << "after : tf = " << tf << std::endl;
@@ -73,7 +81,7 @@ int		main()
 
 	std::cout << "before: AAA = " << AAA << std::endl;
 	std::cout << "before: aaa = " << aaa << std::endl;
-	std::cout << "swap(AAA, aaa)" << std::endl;
+	std::cout << "** swap(AAA, aaa) **" << std::endl;
 	swap(AAA, aaa);
 	std::cout << "after : AAA = " << AAA << std::endl;
 	std::cout << "after : aaa = " << aaa << std::endl;
