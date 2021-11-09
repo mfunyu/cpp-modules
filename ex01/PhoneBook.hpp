4f6d933ea3e9540562ae5add89991a4ac8babe6c
mfunyu <mfunyu@student.42tokyo.jp>
2021-11-09 22:11:24 +0900

    ex01 [clean] trivial cleanup

--- ex01/PhoneBook.hpp
+++ ex01/PhoneBook.hpp
@@ -6,14 +6,14 @@
 /*   By: mfunyu <mfunyu@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/11 00:12:35 by mfunyu            #+#    #+#             */
-/*   Updated: 2021/11/08 15:03:47 by mfunyu           ###   ########.fr       */
+/*   Updated: 2021/11/09 22:10:51 by mfunyu           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PHONEBOOK_HPP
 # define PHONEBOOK_HPP
 
-#include "Contact.hpp"
+# include "Contact.hpp"
 
 # define MAX_CONTACTS 8
 # define NUM_COLS 4
