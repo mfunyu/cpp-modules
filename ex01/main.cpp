002ffbcfd18216cbc4fb1678140b3ebb0d11ae51
mfunyu <mfunyu@student.42tokyo.jp>
2021-11-10 11:59:13 +0900

    ex01 [clean] format, variables

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -6,21 +6,21 @@
 /*   By: mfunyu <mfunyu@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/17 18:29:00 by mfunyu            #+#    #+#             */
-/*   Updated: 2021/10/24 00:45:39 by mfunyu           ###   ########.fr       */
+/*   Updated: 2021/11/10 11:57:43 by mfunyu           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "Zombie.hpp"
 #include <string>
+#include "Zombie.hpp"
 
 Zombie*	zombieHorde( int N, std::string name );
 
 int		main(void)
 {
-	std::string	name = "Julia";
 	int			N = 10;
+	std::string	name = "Julia";
 
-	Zombie*		zombies = zombieHorde(N, name);
+	Zombie*	zombies = zombieHorde(N, name);
 
 	for (int i = 0; i < N; i++) {
 		zombies[i].announce();
