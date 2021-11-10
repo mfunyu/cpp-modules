aecc200c073ff3ec4a01dfad3bc490d0749a654c
mfunyu <mfunyu@student.42tokyo.jp>
2021-11-10 20:13:34 +0900

    ex03 [update] main add details to test

--- ex03/main.cpp
+++ ex03/main.cpp
@@ -6,7 +6,7 @@
 /*   By: mfunyu <mfunyu@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/22 23:19:50 by mfunyu            #+#    #+#             */
-/*   Updated: 2021/11/10 14:47:36 by mfunyu           ###   ########.fr       */
+/*   Updated: 2021/11/10 20:10:34 by mfunyu           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,21 +15,59 @@
 #include "HumanA.hpp"
 #include "HumanB.hpp"
 
+/* -------------------------------------------------------------------------- */
+
+#define COLOR_CYAN "\033[36m"
+#define COLOR_RESET "\033[0m"
+
+void	print(std::string testName)
+{
+	std::cout << COLOR_CYAN << testName
+				<< COLOR_RESET << std::endl;
+}
+
+/* -------------------------------------------------------------------------- */
+
 int main()
 {
 	{
+		print("[ Human A: Case of Bob ]");
+
 		Weapon club = Weapon("crude spiked club");
+		print(" ** Weapon Created ** ");
+		std::cout << "Weapon type: " << club.getType() << std::endl;
+
 		HumanA bob("Bob", club);
+		print(" ## Bob Born with the Weapon ## ");
 		bob.attack();
-		club.setType("some other type of club");
+
+		club.setType("kitchen knife");
+		print(" ** Weapon Type Changed ** ");
+		std::cout << "Weapon type: " << club.getType() << std::endl;
+
+		print(" ## Bob's Weapon Automatically Changed ## ");
 		bob.attack();
 	}
 	{
+		print("\n[ Human B: Case of Jim ]");
+
 		Weapon club = Weapon("crude spiked club");
+		print(" ** Weapon Created ** ");
+		std::cout << club.getType() << " created" << std::endl;
+
 		HumanB jim("Jim");
-		jim.setWeapon(club);
+		print(" ## Jim Born without Weapon ## ");
 		jim.attack();
-		club.setType("some other type of club");
+
+		jim.setWeapon(club);
+		print(" ## Weapon Given to Jim ##  ");
+		jim.attack();
+
+		club.setType("spoon");
+		print(" ** Weapon Type Changed ** ");
+		std::cout << "Weapon type: " << club.getType() << std::endl;
+
+		print(" ## Bob's Weapon Automatically Changed ## ");
 		jim.attack();
 	}
 }
