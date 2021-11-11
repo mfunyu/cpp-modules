e4770dcc6ec52d7056238c85deabc2c8d3492355
mfunyu <mfunyu@student.42tokyo.jp>
2021-11-11 15:41:33 +0900

    ex06 [update] add color print

--- ex06/main.cpp
+++ ex06/main.cpp
@@ -6,13 +6,26 @@
 /*   By: mfunyu <mfunyu@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/23 20:54:26 by mfunyu            #+#    #+#             */
-/*   Updated: 2021/10/24 00:28:13 by mfunyu           ###   ########.fr       */
+/*   Updated: 2021/11/11 15:39:46 by mfunyu           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Karen.hpp"
 #include <iostream>
 #include <string>
+
+/* -------------------------------------------------------------------------- */
+
+#define COLOR_CYAN "\033[36m"
+#define COLOR_RESET "\033[0m"
+
+void	print(std::string typeLevel)
+{
+	std::cout << COLOR_CYAN << "[ " << typeLevel << " ]"
+				<< COLOR_RESET << std::endl;
+}
+
+/* -------------------------------------------------------------------------- */
 
 enum	ComplainType
 {
@@ -48,23 +61,23 @@ int		main(int ac, char **av)
 
 	switch (resolveComplainType(av[1]))
 	{
-		default:
-			std::cout << "[ Probably complaining about insignificant problems ]" << std::endl;
-			break ;
 		case DEBUG:
-			std::cout << "[ DEBUG ]" << std::endl;
+			print("DEBUG");
 			karen.complain("DEBUG");
 			std::cout << std::endl;
 		case INFO:
-			std::cout << "[ INFO ]" << std::endl;
+			print("INFO");
 			karen.complain("INFO");
 			std::cout << std::endl;
 		case WARNING:
-			std::cout << "[ WARNING ]" << std::endl;
+			print("WARNING");
 			karen.complain("WARNING");
 			std::cout << std::endl;
 		case ERROR:
-			std::cout << "[ ERROR ]" << std::endl;
+			print("ERROR");
 			karen.complain("ERROR");
+			break ;
+		default:
+			print("Probably complaining about insignificant problems");
 	}
 }
