da58c7c0827c38753553fda596d72c36dd986ad7
mfunyu <mfunyu@student.42tokyo.jp>
2021-11-11 11:50:40 +0900

    ex03 [fix] segfault protect #7

--- ex03/HumanA.cpp
+++ ex03/HumanA.cpp
@@ -6,11 +6,12 @@
 /*   By: mfunyu <mfunyu@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/22 23:24:36 by mfunyu            #+#    #+#             */
-/*   Updated: 2021/11/10 14:44:06 by mfunyu           ###   ########.fr       */
+/*   Updated: 2021/11/11 11:49:32 by mfunyu           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
+#include <string>
 #include "HumanA.hpp"
 
 HumanA::HumanA(std::string name, Weapon& weapon) :
@@ -19,6 +20,10 @@ HumanA::HumanA(std::string name, Weapon& weapon) :
 HumanA::~HumanA() {}
 
 void	HumanA::attack() {
-	std::cout << _name << " attacks with his " <<\
-				 _weapon.getType() << std::endl;
+	if (!reinterpret_cast<void *>(&_weapon)) {
+		std::cout << _name << " somehow does not have a weapon" << std::endl;
+	} else {
+		std::cout << _name << " attacks with his " <<\
+					 _weapon.getType() << std::endl;
+	}
 }
