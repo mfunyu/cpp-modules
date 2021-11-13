f29c0e8e2eb2bee439b9850a48443063264877e4
mfunyu <mfunyu@student.42tokyo.jp>
2021-11-13 22:35:13 +0900

    ex03 [add] test cases

--- ex03/main.cpp
+++ ex03/main.cpp
@@ -6,7 +6,7 @@
 /*   By: mfunyu <mfunyu@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/13 19:35:58 by mfunyu            #+#    #+#             */
-/*   Updated: 2021/11/13 20:20:22 by mfunyu           ###   ########.fr       */
+/*   Updated: 2021/11/13 22:34:45 by mfunyu           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,14 +15,16 @@
 
 bool	bsp(Point const a, Point const b, Point const c, Point const point);
 
+/* ------------------------------- formatting ------------------------------- */
+
 #define COLOR_GREEN "\033[32m"
 #define COLOR_YELLOW "\033[33m"
 #define COLOR_CYAN "\033[36m"
 #define COLOR_RESET "\033[0m"
 
-void	printPoint(Point const a, Point const b, Point const c, Point const point)
+namespace {
+void	printPoints(Point const a, Point const b, Point const c, Point const point)
 {
-	std::cout << std::endl;
 	std::cout << COLOR_CYAN <<\
 				 "[ " <<\
 				 "(" << a.getX() << ", " << a.getY() << ")" <<\
@@ -34,24 +36,37 @@ void	printPoint(Point const a, Point const b, Point const c, Point const point)
 				 COLOR_RESET << std::endl;
 }
 
+void	printHeader(std::string content)
+{
+	std::cout << std::endl;
+	std::cout << COLOR_CYAN <<\
+				 "*** " << content << " ***" <<\
+				 COLOR_RESET << std::endl;
+}
+} //namespace
+
+/* -------------------------------------------------------------------------- */
+
 void	test(Point const a, Point const b, Point const c, Point const point)
 {
-	printPoint(a, b, c, point);
-	// std::cout << a << std::endl;
-	// std::cout << b << std::endl;
-	// std::cout << c << std::endl;
-	// std::cout << point << std::endl;
-	std::cout << "result: " << bsp(a, b, c, point) << std::endl;
+	printPoints(a, b, c, point);
+
+	std::cout << "result: " <<\
+				 (bsp(a, b, c, point) ? "true" : "false") << std::endl;
 }
 
 int		main()
 {
-	Point	a(0, 0);
-	Point	b(0, 1);
-	Point	c(1, 0);
-	Point	point1(0.5, 0.5);
-	Point	point2(1.5, 0.5);
+	printHeader("On edge");
+	test(Point(0, 0), Point(0, 1), Point(1, 0), Point(0.5, 0.5));
 
-	test(a, b, c, point1);
-	test(a, b, c, point2);
+	printHeader("At vertex");
+	test(Point(0, 0), Point(0, 1), Point(1, 0), Point(0, 0));
+	test(Point(1, 1), Point(0, 1), Point(1, 0), Point(0, 1));
+
+	printHeader("Outside");
+	test(Point(0, 0), Point(0, 1), Point(1, 0), Point(1.2, 0.7));
+
+	printHeader("Inside");
+	test(Point(0, 0), Point(0, 1), Point(1, 0), Point(0.4, 0.3));
 }
