eb1cf608fc83a5d03b58912be0738a3ea6bb4bb3
mfunyu <mfunyu@student.42tokyo.jp>
2021-11-13 23:38:52 +0900

    ex03 [clean] del cout

--- ex03/bsp.cpp
+++ ex03/bsp.cpp
@@ -6,7 +6,7 @@
 /*   By: mfunyu <mfunyu@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/13 19:39:53 by mfunyu            #+#    #+#             */
-/*   Updated: 2021/11/13 23:33:13 by mfunyu           ###   ########.fr       */
+/*   Updated: 2021/11/13 23:38:19 by mfunyu           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,9 +22,6 @@ bool	bsp(Point const a, Point const b, Point const c, Point const point)
 	bc = (point - b).crossP(c - b);
 	ca = (point - c).crossP(a - c);
 
-	std::cout << ab << std::endl;
-	std::cout << bc << std::endl;
-	std::cout << ca << std::endl;
 	if (ab < 0 && bc < 0 && ca < 0) {
 		return true;
 	} else if (ab > 0 && bc > 0 && ca > 0) {
