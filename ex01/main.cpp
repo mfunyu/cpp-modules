48a98edc99759ceefd6cefc2d4a36e4f1305e8a8
mfunyu <mfunyu@student.42tokyo.jp>
2021-11-15 00:08:28 +0900

    ex01 [brushup] main

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: mfunyu <mfunyu@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/07 18:31:07 by mfunyu            #+#    #+#             */
-/*   Updated: 2021/11/10 19:58:29 by mfunyu           ###   ########.fr       */
+/*   Updated: 2021/11/15 00:06:38 by mfunyu           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,20 +14,12 @@
 #include <iostream>
 #include "iter.hpp"
 
-namespace {
+/* ---------------------------------- print --------------------------------- */
+
 #define COLOR_CYAN "\033[36m"
 #define COLOR_RESET "\033[0m"
 
-void	strToUpper(std::string const & str)
-{
-	std::locale	locale;
-
-	for (int i = 0; str[i]; i++) {
-		std::cout << std::toupper(str[i], locale);
-	}
-	std::cout << std::endl;
-}
-
+namespace {
 void	printLst(std::string const* lst)
 {
 	for (int i = 0; !lst[i].empty(); i++) {
@@ -40,34 +32,77 @@ void	print(std::string testName)
 	std::cout << COLOR_CYAN << testName
 				<< COLOR_RESET << std::endl;
 }
+
+void	printHelp()
+{
+	std::cout << "usage: ./a.out [testname]\n" << std::endl;
+	std::cout << "  \"testnames\"" << std::endl;
+	std::cout << "\t\"original\"\t-> my original main" << std::endl;
+	std::cout << "\t\"review\"\t-> main for review (using class Awesome)" << std::endl;
+}
 } /* namespace */
 
-int		main()
-{
-	{
-		std::string		str[4] = { "aaaaaaaaa",
-									"bbbbbbbbb",
-									"ccccccccc",
-								};
-		print("[ Before ]");
-		printLst(str);
-		print("** Call func **");
-		iter<std::string>(str, 3, strToUpper);
-		print("[ After ]");
-		printLst(str);
-	}
-	std::cout << std::endl;
-	{
-		const std::string	const_str[4] = { "aaaaaaaaa",
-									"bbbbbbbbb",
-									"ccccccccc",
-								};
+/* --------------------------------- review --------------------------------- */
 
-		print("[ Before ]");
-		printLst(const_str);
-		print("** Call func **");
-		iter<const std::string>(const_str, 3, strToUpper);
-		print("[ After ]");
-		printLst(const_str);
+namespace review {
+class Awesome
+{
+	public:
+		Awesome( void ) : _n( 42 ) { return; }
+		int		get( void ) const { return this->_n; }
+
+	private:
+		int _n;
+};
+
+std::ostream &	operator<<( std::ostream & o, Awesome const & rhs ) { o << rhs.get(); return o; }
+
+template< typename T >
+void	print( T const & x ) { std::cout << x << std::endl; return; }
+
+int		review_main()
+{
+	int tab[] = { 0, 1, 2, 3, 4 };
+	Awesome tab2[5];
+
+	iter( tab, 5, print );
+	iter( tab2, 5, print );
+	return 0;
+}
+} // namespace review
+
+/* ------------------------------ original test ----------------------------- */
+
+namespace {
+template <typename T>
+void	printLen(T const & x)
+{
+	size_t	len = 0;
+	for (; x[len]; len++) {}
+	std::cout << len << std::endl;
+}
+
+void	my_main()
+{
+	std::string		str[4] = { "aaaaa", "bbbbbbbbb", "cc" };
+	print("[ Before ]");
+	printLst(str);
+	print("** Call func **");
+	iter<std::string>(str, 3, printLen);
+	print("[ After ]");
+	printLst(str);
+}
+} // namespace
+
+int		main(int ac, char **av)
+{
+	std::string	test = (ac > 1 ? av[1] : "");
+
+	if (test == "original") {
+		my_main();
+	} else if (test == "review") {
+		review::review_main();
+	} else {
+		printHelp();
 	}
 }
