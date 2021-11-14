cde24a7a73c3247e70e0dfe60a2aef840e94ef69
mfunyu <mfunyu@student.42tokyo.jp>
2021-11-14 11:56:27 +0900

    ex00 [fix] int -> unsigned int

--- ex00/ClapTrap.hpp
+++ ex00/ClapTrap.hpp
@@ -6,7 +6,7 @@
 /*   By: mfunyu <mfunyu@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/27 12:26:51 by mfunyu            #+#    #+#             */
-/*   Updated: 2021/10/29 13:03:24 by mfunyu           ###   ########.fr       */
+/*   Updated: 2021/11/14 11:55:41 by mfunyu           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,8 +20,8 @@ class ClapTrap
 	private:
 		std::string		_name;
 		unsigned int	_hitPoints;
-		int				_energyPoints;
-		int				_attackDamage;
+		unsigned int	_energyPoints;
+		unsigned int	_attackDamage;
 
 	public:
 		ClapTrap();
