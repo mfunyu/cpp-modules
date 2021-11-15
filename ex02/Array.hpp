20922ab2240c69c85a87fe52e80e4f766a15b70d
mfunyu <mfunyu@student.42tokyo.jp>
2021-11-15 12:42:22 +0900

    ex02 [clean] move some definitions out of class bracket

--- ex02/Array.hpp
+++ ex02/Array.hpp
@@ -6,7 +6,7 @@
 /*   By: mfunyu <mfunyu@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/07 18:32:45 by mfunyu            #+#    #+#             */
-/*   Updated: 2021/11/15 12:27:33 by mfunyu           ###   ########.fr       */
+/*   Updated: 2021/11/15 12:41:02 by mfunyu           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,44 +19,61 @@ template <typename T>
 class Array
 {
   private:
-	T*				_array;
+	T *				_array;
 	unsigned int	_size;
 
   public:
 	Array<T>() : _size(0) { _array = new T[0](); }
 	Array<T>(unsigned int n) : _size(n) { _array = new T[n]();}
 
-	Array<T>(Array<T> const &other) {
-		_size = other._size;
-		_array = new T[_size]();
+	Array<T>(Array<T> const &other);
 
-		for (unsigned int i = 0; i < other._size; i++) {
-			_array[i] = other._array[i];
-		}
-	}
-
-	Array<T>	&operator=(Array<T> const &other) {
-		if (this != &other) {
-			delete [] _array;
-			_array = new T[other._size]();
-			_size = other._size;
-			for (unsigned int i = 0; i < other._size; i++) {
-				_array[i] = other._array[i];
-			}
-		}
-		return *this;
-	}
-
-	T	&operator[](unsigned int i) {
-		if (i < 0 || i >= _size) {
-			throw std::out_of_range("index out of range");
-		}
-		return _array[i];
-	}
+	Array<T>	&operator=(Array<T> const &other);
+	T			&operator[](unsigned int i);
 
 	unsigned int	size(void) const { return _size; }
 
 	~Array<T>() { delete [] _array; }
 };
+
+/* ------------------------------- definitions ------------------------------ */
+
+/* Copy constructor */
+template <typename T>
+Array<T>::Array(Array<T> const &other)
+{
+	_size = other._size;
+	_array = new T[_size]();
+
+	for (unsigned int i = 0; i < other._size; i++) {
+	_array[i] = other._array[i];
+	}
+}
+
+/* Assignment operator = overload */
+template <typename T>
+Array<T> &	Array<T>::operator=(Array<T> const &other)
+{
+	if (this != &other) {
+		delete [] _array;
+		_size = other._size;
+		_array = new T[other._size]();
+
+		for (unsigned int i = 0; i < other._size; i++) {
+			_array[i] = other._array[i];
+		}
+	}
+	return *this;
+}
+
+/* Subscript operator [] overload */
+template <typename T>
+T &	Array<T>::operator[](unsigned int i)
+{
+	if (i < 0 || i >= _size) {
+		throw std::out_of_range("index out of range");
+	}
+	return _array[i];
+}
 
 #endif /* ARRAY_HPP */
