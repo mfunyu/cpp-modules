972a149e7b0d50d4fbb5e916669e5e24a458a532
mfunyu <mfunyu@student.42tokyo.jp>
2021-11-15 18:48:17 +0900

    ex02 [brushup] main

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -9,30 +9,47 @@
 #define COLOR_CYAN "\033[36m"
 #define COLOR_RESET "\033[0m"
 
+void	printHelp()
+{
+	std::cout << "usage: ./a.out [testname]\n" << std::endl;
+	std::cout << "  \"testnames\"" << std::endl;
+	std::cout << "\t\"subject\" -> subject test" << std::endl;
+	std::cout << "\t\"constructor\" -> constructor test" << std::endl;
+	std::cout << "\t\"copyC\" -> copy constructor test" << std::endl;
+	std::cout << "\t\"=\" -> assignment operator test" << std::endl;
+	std::cout << "\t\"[]\" -> subscription operator test" << std::endl;
+	std::cout << "\t\"size\" -> size() test" << std::endl;
+}
+
 void	printTestName(std::string test){
-	std::cout << std::endl;
 	std::cout << COLOR_CYAN << "****** Test " << test <<\
 				 " ******" << COLOR_RESET << std::endl;
 }
 
 template <typename T>
-void	printElem(int i, T elem, int total) {
-	std::cout << "[" << i << "]: " << elem << " " << std::flush;
-	if (i == total - 1) {
-		std::cout << std::endl;
-	}
-}
-
-template <typename T>
-void	printList(std::string title, Array<T> elem, int total) {
+void	printList(std::string title, Array<T> & elem, int total) {
 	std::cout << std::left << std::setw(9) << title << "| ";
 	for (int i = 0; i < total; i++) {
-		printElem(i, elem[i], total);
+		std::cout << "[" << i << "]: " << elem[i] << " " << std::flush;
 	}
+	std::cout << std::endl;
 }
 
-void	printTestInfo(std::string testinfo) {
-	std::cout << testinfo << ": ";
+#include <cxxabi.h>
+std::string getNameByTypeInfo(std::type_info const& typeInfo)
+{
+	char*	charName;
+	int		status = 0;
+	charName = abi::__cxa_demangle(typeInfo.name(), 0, 0, &status);
+	std::string	strName(charName);
+	std::free(charName);
+	return strName;
+}
+
+void	printTestInfo(std::string testinfo, std::type_info const& typeInfo) {
+	std::cout << COLOR_CYAN << "[ " << testinfo << \
+				 " <" << getNameByTypeInfo(typeInfo) << ">" << \
+				 " ]" << COLOR_RESET << std::endl;
 }
 
 void	printKO() {
@@ -66,49 +83,40 @@ int		subject_main()
 		Array<int> test(tmp);
 	}
 
-	for (int i = 0; i < MAX_VAL; i++)
-	{
-		if (mirror[i] != numbers[i])
-		{
+	for (int i = 0; i < MAX_VAL; i++) {
+		if (mirror[i] != numbers[i]) {
 			std::cerr << "didn't save the same value!!" << std::endl;
 			return 1;
 		}
 	}
-	std::cout << "same values are saved" << std::endl;
-	try
-	{
+	try {
 		numbers[-2] = 0;
-	}
-	catch (const std::exception &e)
-	{
+	} catch (const std::exception &e) {
 		std::cerr << e.what() << '\n';
 	}
-	try
-	{
+	try {
 		numbers[MAX_VAL] = 0;
-	}
-	catch (const std::exception &e)
-	{
+	} catch (const std::exception &e) {
 		std::cerr << e.what() << '\n';
 	}
 
-	for (int i = 0; i < MAX_VAL; i++)
-	{
+	for (int i = 0; i < MAX_VAL; i++) {
 		numbers[i] = rand();
 	}
-	delete[] mirror; //
+	delete[] mirror;
 
 	return 0;
 }
 
 template <typename T>
-void	testSize(std::string type)
+void	testSize(unsigned int size)
 {
-	unsigned int	n = 8;
-	Array<T> test(n);
-	printTestInfo("Accurate size returned <" + type + ">");
+	printTestInfo("Accurate size returned", typeid(int));
+	Array<T> test(size);
 
-	if (test.size() != n) {
+	std::cout << "size = " << test.size() << std::endl;
+
+	if (test.size() != size) {
 		printKO();
 		return ;
 	}
@@ -116,88 +124,78 @@ void	testSize(std::string type)
 }
 
 template <typename T>
-void	testSubscriptOperator(std::string type)
+void	testSubscriptOperator(int size, int index)
 {
-	int		n = 8;
-	Array<T> test(n);
-	std::string testInfo = "Exception thrown at right index <" + type + ">";
+	printTestInfo("Exception thrown at right index", typeid(T));
+
+	Array<T> array(size);
+
 	try {
-		for (int i = 0; i < n + 10; i++) {
-			test[i] = i * 10;
-			if (i >= n) {
-				printKO();
-				return ;
-			}
-		}
+		std::cout << "array[" << index << "] = " << std::flush;
+		std::cout << array[index] << std::endl;
 	} catch (std::exception &e){
-		printOK();
+		std::cout << e.what() << std::endl;;
 	}
 }
 
 template <typename T>
-void	testAssignmentOperator(std::string type)
+void	testAssignmentOperator(int dst_size, int src_size)
 {
-	int		n = 8;
-	int		m = 4;
-	Array<T> src(n);
-	Array<T> dst(m);
-	std::string testInfo = "Running deep copy <" + type + ">";
-	dst = src;
-	for (int i = 0; i < n; i++) {
-		dst[i] = i + 'A';
-		if (dst[i] == src[i]) {
-			printKO();
-			return ;
-		}
+	printTestInfo("Running deep copy", typeid(T));
+
+	Array<T> src(src_size);
+	for (int i = 0; i < src_size; i++) {
+		src[i] = i + 'a';
 	}
-	printList<T>("dst", dst, n);
-	printList<T>("src", src, n);
+
+	Array<T> dst(dst_size);
+	for (int i = 0; i < dst_size; i++) {
+		dst[i] = i + 'A';
+	}
+
+	printList<T>("dst", dst, dst_size);
+	printList<T>("src", src, src_size);
+	dst = src;
+	printList<T>("dst", dst, src_size);
+	printList<T>("src", src, src_size);
 	printOK();
 }
 
 template <typename T>
-void	testCopyConstructor(std::string type)
+void	testCopyConstructor(int n)
 {
-	int		n = 5;
+	printTestInfo("Running deep copy", typeid(T));
+
 	Array<T> original(n);
-	Array<T> copy(original);
-	std::string testInfo = "Running deep copy <" + type + ">";
 	for (int i = 0; i < n; i++) {
 		original[i] = i + 'a';
-		if (original[i] == copy[i]) {
+	}
+
+	Array<T> copy(original);
+
+	printList<T>("original", original, n);
+	printList<T>("copy", copy, n);
+
+	for (int i = 0; i < n; i++) {
+		if (original[i] != copy[i]) {
 			printKO();
 			return ;
 		}
 	}
-	printList<T>("original", original, n);
-	printList<T>("copy", copy, n);
 	printOK();
 }
 
 template <typename T>
-void	testConstructor(std::string type)
+void	testConstructor(int len)
 {
-	int		n = 10;
-	Array<T> numbers(n);
-	std::string testInfo = "Initialized to default <" + type + ">";
-	for (int i = 0; i < n; i++) {
-		printElem<T>(i, numbers[i], n);
-		if (numbers[i] != 0) {
-			printKO();
-			return ;
-		}
-	}
-	printOK();
-}
+	printTestInfo("Initialized to default", typeid(T));
 
-void	testConstructor()
-{
-	std::string testInfo = "Initialized to default <std::string>";
-	int		n = 10;
-	Array<std::string> strs(n);
-	for (int i = 0; i < n; i++) {
-		printElem<std::string>(i, strs[i], n);
-		if (!strs[i].empty()) {
+	Array<T> zeroInitializedArray(len);
+
+	printList("zero", zeroInitializedArray, len);
+
+	for (int i = 0; i < len; i++) {
+		if (zeroInitializedArray[i]) {
 			printKO();
 			return ;
 		}
@@ -206,38 +204,42 @@ void	testConstructor()
 }
 } /* namespace */
 
-int		main()
+int		main(int ac, char **av)
 {
 	try {
-	subject_main();
+		std::string	test = (ac > 1 ? av[1] : "");
 
-	printTestName("Constructor");
-	testConstructor<int>("int");
-	testConstructor<float>("float");
-	testConstructor<const unsigned char>("const unsigned char");
-	testConstructor();
-
-	printTestName("Copy Constructor");
-	testCopyConstructor<int>("int");
-	testCopyConstructor<float>("float");
-	testCopyConstructor<double>("double");
-	testCopyConstructor<std::string>("std::string");
-
-	printTestName("Assignment Operator = ");
-	testAssignmentOperator<unsigned int>("unsigned int");
-	testAssignmentOperator<char>("char");
-	testAssignmentOperator<std::string>("std::string");
-
-	printTestName("Subscript Operator []");
-	testSubscriptOperator<float>("float");
-	testSubscriptOperator<unsigned char>("unsigned char");
-	testSubscriptOperator<int64_t>("int64_t");
-
-	printTestName("size");
-	testSize<const float>("const float");
-	testSize<unsigned char>("unsigned char");
-	testSize<const std::string>("const std::string");
-
+		if (test == "subject") {
+			subject_main();
+		} else if (test == "constructor") {
+			printTestName("Constructor");
+			testConstructor<int>(10);
+			testConstructor<float>(8);
+			testConstructor<const unsigned char>(1);
+		} else if (test == "copyC") {
+			printTestName("Copy Constructor");
+			testCopyConstructor<int>(5);
+			testCopyConstructor<float>(8);
+			testCopyConstructor<double>(3);
+			testCopyConstructor<std::string>(6);
+		} else if (test == "=") {
+			printTestName("Assignment Operator = ");
+			testAssignmentOperator<unsigned int>(3, 2);
+			testAssignmentOperator<char>(0, 12);
+			testAssignmentOperator<std::string>(5, 5);
+		} else if (test == "[]") {
+			printTestName("Subscript Operator []");
+			testSubscriptOperator<float>(7, 3);
+			testSubscriptOperator<unsigned char>(5, -1);
+			testSubscriptOperator<int64_t>(2, 5);
+		} else if (test == "size") {
+			printTestName("size");
+			testSize<const float>(3);
+			testSize<unsigned char>(4);
+			testSize<const std::string>(5);
+		} else {
+			printHelp();
+		}
 	} catch (std::exception &e) {
 		std::cerr << e.what() << std::endl;
 	}
