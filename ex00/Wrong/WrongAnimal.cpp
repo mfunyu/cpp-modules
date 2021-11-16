935679a6ba41a6f30f9bb62ece014d579024fe07
mfunyu <mfunyu@student.42tokyo.jp>
2021-11-16 16:52:29 +0900

    ex00 [fix] class Wrong output #5

--- ex00/Wrong/WrongAnimal.cpp
+++ ex00/Wrong/WrongAnimal.cpp
@@ -6,19 +6,22 @@
 /*   By: mfunyu <mfunyu@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/29 14:55:24 by mfunyu            #+#    #+#             */
-/*   Updated: 2021/11/02 12:33:40 by mfunyu           ###   ########.fr       */
+/*   Updated: 2021/11/16 16:50:21 by mfunyu           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "WrongAnimal.hpp"
 #include <iostream>
+#include <string>
 
-WrongAnimal::WrongAnimal()
+WrongAnimal::WrongAnimal() : _type("WrongAnimal")
 {
+	std::cout << "Wrong Animal default constructor called" << std::endl;
 }
 
 WrongAnimal::~WrongAnimal()
 {
+	std::cout << "Wrong Animal destructor called" << std::endl;
 }
 
 WrongAnimal::WrongAnimal(const WrongAnimal &other)
@@ -37,6 +40,7 @@ WrongAnimal &WrongAnimal::operator=(const WrongAnimal &other)
 
 WrongAnimal::WrongAnimal(std::string type) : _type(type)
 {
+	std::cout << "Wrong Animal constructor called" << std::endl;
 }
 
 std::string WrongAnimal::getType() const
--- ex00/Wrong/WrongCat.cpp
+++ ex00/Wrong/WrongCat.cpp
@@ -6,19 +6,22 @@
 /*   By: mfunyu <mfunyu@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/29 15:09:24 by mfunyu            #+#    #+#             */
-/*   Updated: 2021/11/02 12:33:57 by mfunyu           ###   ########.fr       */
+/*   Updated: 2021/11/16 16:50:38 by mfunyu           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "WrongCat.hpp"
 #include "WrongAnimal.hpp"
+#include <iostream>
 
 WrongCat::WrongCat() : WrongAnimal("WrongCat")
 {
+	std::cout << "Wrong Cat default constructor called" << std::endl;
 }
 
 WrongCat::~WrongCat()
 {
+	std::cout << "Wrong Cat destructor called" << std::endl;
 }
 
 WrongCat::WrongCat(const WrongCat &other)
