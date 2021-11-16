b2c6521d9a5c985ac71049b03c9055b1db368185
mfunyu <mfunyu@student.42tokyo.jp>
2021-11-16 23:39:34 +0900

    ex03 [update] add constructor msgs

--- ex03/materia_src/MateriaSource.cpp
+++ ex03/materia_src/MateriaSource.cpp
@@ -6,14 +6,16 @@
 /*   By: mfunyu <mfunyu@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/16 22:56:10 by mfunyu            #+#    #+#             */
-/*   Updated: 2021/11/16 23:01:41 by mfunyu           ###   ########.fr       */
+/*   Updated: 2021/11/16 23:30:42 by mfunyu           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "MateriaSource.hpp"
+#include <iostream>
 
 MateriaSource::MateriaSource()
 {
+	std::cout << "MateriaSource default constructor called" << std::endl;
 	for (int i = 0; i < kMaxMaterials; i++)
 	{
 		_memory[i] = NULL;
@@ -22,6 +24,7 @@ MateriaSource::MateriaSource()
 
 MateriaSource::~MateriaSource()
 {
+	std::cout << "MateriaSource destructor called" << std::endl;
 	for (int i = 0; i < kMaxMaterials; i++)
 	{
 		delete _memory[i];
@@ -30,12 +33,14 @@ MateriaSource::~MateriaSource()
 
 MateriaSource::MateriaSource(const MateriaSource &other)
 {
+	std::cout << "MateriaSource copy constructor called" << std::endl;
 	MateriaSource();
 	*this = other;
 }
 
 MateriaSource	&MateriaSource::operator=(const MateriaSource &other)
 {
+	std::cout << "MateriaSource assignment operator called" << std::endl;
 	if (this != &other)
 	{
 		for (int i = 0; i < kMaxMaterials; i++)
