4774b5621a4b91bea28b266a4796e22dc0f44daf
mfunyu <mfunyu@student.42tokyo.jp>
2021-11-16 16:53:15 +0900

    ex00 [update] wrong_main

--- ex00/Wrong/wrong_main.cpp
+++ ex00/Wrong/wrong_main.cpp
@@ -1,27 +1,49 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.cpp                                           :+:      :+:    :+:   */
+/*   wrong_main.cpp                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mfunyu <mfunyu@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/29 15:16:58 by mfunyu            #+#    #+#             */
-/*   Updated: 2021/10/30 17:54:20 by mfunyu           ###   ########.fr       */
+/*   Updated: 2021/11/16 16:51:06 by mfunyu           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "WrongAnimal.hpp"
 #include "WrongCat.hpp"
 
+#include <iostream>
+#include <string>
+
+/* ------------------------------- formatting ------------------------------- */
+
+#define COLOR_CYAN "\033[36m"
+#define COLOR_RESET "\033[0m"
+
+namespace {
+void	printSubHeader(std::string content)
+{
+	std::cout << COLOR_CYAN <<\
+				 "[ " << content << " ]" <<\
+				 COLOR_RESET << std::endl;
+}
+} // namespace
+
+/* -------------------------------------------------------------------------- */
 int main()
 {
-	const WrongAnimal* meta = new WrongAnimal();
-	const WrongAnimal* i = new WrongCat();
+	printSubHeader("WrongAnimal* animal = new WrongAnimal()");
+	const WrongAnimal* animal = new WrongAnimal();
+	std::cout << "getType(): " << animal->getType() << " " << std::endl;
+	std::cout << "makeSound(): ";
+	animal->makeSound();
+	delete animal;
 
-	std::cout << i->getType() << " " << std::endl;
-	i->makeSound(); //will output the cat sound!
-	meta->makeSound();
-
-	delete meta;
-	delete i;
+	printSubHeader("WrongAnimal* A_cat = new WrongCat()");
+	const WrongAnimal* A_cat = new WrongCat();
+	std::cout << "getType(): " << A_cat->getType() << " " << std::endl;
+	std::cout << "makeSound(): ";
+	A_cat->makeSound();
+	delete A_cat;
 }
