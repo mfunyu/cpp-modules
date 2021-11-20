ecc51fe445176fcbf2e25b0d04ae3fb06f085cfc
mfunyu <mfunyu@student.42tokyo.jp>
2021-11-20 13:29:51 +0900

    ex01 [add] main

---
+++ ex01/main.cpp
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mfunyu <mfunyu@student.42tokyo.jp>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/17 19:08:36 by mfunyu            #+#    #+#             */
+/*   Updated: 2021/11/20 13:28:31 by mfunyu           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "span.hpp"
+#include <iostream>
+
+void	subject_main(void)
+{
+	Span sp = Span(5);
+
+	sp.addNumber(5);
+	sp.addNumber(3);
+	sp.addNumber(17);
+	sp.addNumber(9);
+	sp.addNumber(11);
+	std::cout << sp.shortestSpan() << std::endl;
+	std::cout << sp.longestSpan() << std::endl;
+}
+
+int		main(int ac, char **av)
+{
+	std::string	test = (ac > 1 ? av[1] : "");
+
+	if (test.empty()) {
+		subject_main();
+	} else {
+	}
+}
