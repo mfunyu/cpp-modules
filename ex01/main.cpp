5b258a59a3b82859fee7d89626b5b9445db8d237
mfunyu <mfunyu@student.42tokyo.jp>
2021-11-21 21:14:18 +0900

    ex01 [update] main add formatting

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: mfunyu <mfunyu@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/06 17:10:50 by mfunyu            #+#    #+#             */
-/*   Updated: 2021/11/07 00:02:14 by mfunyu           ###   ########.fr       */
+/*   Updated: 2021/11/21 21:13:38 by mfunyu           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,18 +14,29 @@
 #include <cstdint>
 #include <iostream>
 
-/****************** FOR PRINT and FORMATTING ******************/
+/* ------------------------------- formatting ------------------------------- */
 
-# define SET_COLOR "\033[36m"
-# define RESET_COLOR "\033[m"
+#define COLOR_CYAN "\033[36m"
+#define COLOR_RESET "\033[0m"
 
-void	printTestInfo(std::string testContent)
+namespace {
+void	printHeader(std::string content)
 {
-	std::cout << SET_COLOR "/*** Test: " << testContent <<\
-				" ***/" RESET_COLOR << std::endl;
+	std::cout << std::endl;
+	std::cout << COLOR_CYAN <<\
+				 "*** " << content << " ***" <<\
+				 COLOR_RESET << std::endl;
 }
 
-/******************    Requied Functions    ******************/
+void	printSubHeader(std::string content)
+{
+	std::cout << COLOR_CYAN <<\
+				 "[ " << content << " ]" <<\
+				 COLOR_RESET << std::endl;
+}
+} // namespace
+
+/* -------------------------------------------------------------------------- */
 
 uintptr_t serialize(Data* ptr)
 {
@@ -40,33 +51,36 @@ Data* deserialize(uintptr_t raw)
 int		main()
 {
 	{
-		printTestInfo("Data test(\"test\");");
+		printHeader("Data test(\"test\");");
 
 		Data	test("test");
 
 		std::cout << "Ptr Before: " << &test << std::endl;
+		std::cout << "func getContent() : " << \
+					 test.getContent() << std::endl;
 
+		printSubHeader("* serialize and deserialize *");
 		uintptr_t	ptr = serialize(&test);
 		Data*		deserialized = deserialize(ptr);
 
 		std::cout << "Ptr After : " << deserialized << std::endl;
-
 		std::cout << "func getContent() : " << \
 					 deserialized->getContent() << std::endl;
 	}
-	std::cout << std::endl;
 	{
-		printTestInfo("Data* test = new Data(\"content\");");
+		printHeader("Data* test = new Data(\"content\");");
 
 		Data*	test = new Data("content");
 
 		std::cout << "Ptr Before: " << test << std::endl;
+		std::cout << "func getContent() : " << \
+					 test->getContent() << std::endl;
 
+		printSubHeader("* serialize and deserialize *");
 		uintptr_t	ptr = serialize(test);
 		Data*		deserialized = deserialize(ptr);
 
 		std::cout << "Ptr After : " << deserialized << std::endl;
-
 		std::cout << "func getContent() : " << \
 					 deserialized->getContent() << std::endl;
 
