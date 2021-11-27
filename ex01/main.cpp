6e0060742548c9d3eb224be62cd9c98eef7e5fe6
mfunyu <mfunyu@student.42tokyo.jp>
2021-11-27 14:10:45 +0900

    ex01 [update] main *estSpan() tests

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: mfunyu <mfunyu@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/17 19:08:36 by mfunyu            #+#    #+#             */
-/*   Updated: 2021/11/26 00:06:41 by mfunyu           ###   ########.fr       */
+/*   Updated: 2021/11/27 14:10:25 by mfunyu           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,9 +26,18 @@ void printHeader(std::string content)
 			  << std::endl;
 }
 
+/*
 void printSubHeader(std::string content)
 {
 	std::cout << COLOR_CYAN << "[ " << content << " ]" << COLOR_RESET
+			  << std::endl;
+}
+*/
+
+void printSize(Span const& test)
+{
+	std::cout << COLOR_CYAN << "[ size = " << test.getSize() << ", "
+			  << "maxsize = " << test.getMaxSize() << " ]" << COLOR_RESET
 			  << std::endl;
 }
 } // namespace
@@ -50,46 +59,66 @@ void test(void)
 }
 } // namespace subject
 
+void shortestSpanTest(Span test)
+{
+	try {
+		printSize(test);
+		std::cout << test.shortestSpan() << std::endl;
+	} catch (const std::exception& e) {
+		std::cerr << e.what() << std::endl;
+	}
+}
+
+void longestSpanTest(Span test)
+{
+	try {
+		printSize(test);
+		std::cout << test.longestSpan() << std::endl;
+	} catch (const std::exception& e) {
+		std::cerr << e.what() << std::endl;
+	}
+}
+
 void exception_tests()
 {
-	printHeader("Exception Tests");
-	try {
-		std::cout << tooShort.shortestSpan() << std::endl;
-	} catch (const std::exception& e) {
-		std::cerr << e.what() << std::endl;
-	}
+	{
+		printHeader("test shortestSpan()");
 
-	tooShort.addNumber(21);
-	try {
-		printSubHeader("size = 1");
-		std::cout << tooShort.shortestSpan() << std::endl;
-	} catch (const std::exception& e) {
-		std::cerr << e.what() << std::endl;
-	}
+		shortestSpanTest(Span(3));
+		shortestSpanTest(Span(2));
+		shortestSpanTest(Span(1));
 
-	tooShort.addNumber(42);
-	try {
-		printSubHeader("size = 2");
-		std::cout << tooShort.shortestSpan() << std::endl;
-	} catch (const std::exception& e) {
-		std::cerr << e.what() << std::endl;
+		Span nonEmpty = Span(3);
+		nonEmpty.addNumber(42); // 42
+		shortestSpanTest(nonEmpty);
+		nonEmpty.addNumber(12); // 42, 12
+		shortestSpanTest(nonEmpty);
+		nonEmpty.addNumber(1); // 42, 12 ,1
+		shortestSpanTest(nonEmpty);
 	}
+	{
+		printHeader("test longestSpan()");
 
-	tooShort.addNumber(-42);
-	try {
-		printSubHeader("size = 3");
-		std::cout << tooShort.shortestSpan() << std::endl;
-	} catch (const std::exception& e) {
-		std::cerr << e.what() << std::endl;
+		longestSpanTest(Span(3));
+		longestSpanTest(Span(2));
+		longestSpanTest(Span(1));
+
+		Span nonEmpty = Span(3);
+		nonEmpty.addNumber(42); // 42
+		longestSpanTest(nonEmpty);
+		nonEmpty.addNumber(12); // 42, 0
+		longestSpanTest(nonEmpty);
+		nonEmpty.addNumber(1); // 42, 0 ,1
+		longestSpanTest(nonEmpty);
 	}
 }
 
 int main(int ac, char** av)
 {
 	try {
-	std::string test = (ac > 1 ? av[1] : "");
+		std::string test = (ac > 1 ? av[1] : "");
 
-	if (test.empty()) {
+		if (test.empty()) {
 			subject::test();
 		} else if (test == "all") {
 			exception_tests();
