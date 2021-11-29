417bfb859a5239aac6331b3f3f3ef22a269f186c
mfunyu <mfunyu@student.42tokyo.jp>
2021-11-29 14:53:48 +0900

    ex01 [add] template addNumber overload #5

--- ex01/span.hpp
+++ ex01/span.hpp
@@ -6,7 +6,7 @@
 /*   By: mfunyu <mfunyu@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/17 19:05:20 by mfunyu            #+#    #+#             */
-/*   Updated: 2021/11/28 18:58:07 by mfunyu           ###   ########.fr       */
+/*   Updated: 2021/11/29 14:47:39 by mfunyu           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,7 @@
 #define SPAN_HPP
 
 #include <set>
+#include <iterator>
 
 class Span
 {
@@ -30,6 +31,8 @@ class Span
 	Span& operator=(Span const& other);
 
 	void addNumber(int number);
+	template <class RandomAccessIterator>
+	void addNumber(RandomAccessIterator first, RandomAccessIterator last);
 
 	unsigned int shortestSpan();
 	unsigned int longestSpan();
@@ -41,6 +44,21 @@ class Span
   private:
 	Span();
 };
+
+template <class RandomAccessIterator>
+void Span::addNumber(RandomAccessIterator first, RandomAccessIterator last)
+{
+	if (_size >= _N) {
+		throw std::length_error("Span is full");
+	}
+	unsigned int len = std::distance(first, last);
+	if (_size + len > _N) {
+		throw std::length_error("Out of limits");
+	}
+	_size += len;
+	_array.insert(first, last);
+}
+
 
 std::ostream& operator<<(std::ostream& os, Span const& span);
 
